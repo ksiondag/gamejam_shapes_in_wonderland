4d918b8ff2b02e7aeeca5d1fa259360f8f562260
Gregory Ksionda <ksiondag846@gmail.com>
2014-11-08 19:08:38 +0000

    Successfully implementing "elastic" collisions

--- js/lib/movement.js
+++ js/lib/movement.js
@@ -1,3 +1,22 @@
+
+var direction = function( number ) {
+    if( number !== 0 ) {
+        return number/Math.abs(number);
+    }
+
+    return 0;
+};
+
+var sameDir = function( x, y ){
+
+    if( direction(x) === direction(y) ) {
+        return true;
+    }
+
+    return false;
+
+};
+
 Crafty.c("Velocity", {
     vx:0,
     vy:0,
@@ -13,29 +32,90 @@ Crafty.c("Velocity", {
         this.vy = vy; 
 
         return this;
+    },
+
+    xDir: function() {
+        return direction( this.vx );
+    },
+
+    yDir: function() {
+        return direction( this.vy );
     }
 
 });
 
 Crafty.c("Bounce", {
-    is_hit: false,
     init: function() {
+        this.addComponent( "Velocity" );
         this.addComponent( "Collision" );
 
         this.onHit(
-            "Friend",
-            function() {
-                if( this.is_hit === false ) {
-                    this.vx *= -1;
-                    this.vy *= -1;
+            "Solid",
+            function( collisions ) {
+                for( var i in collisions ) {
+                    var collision = collisions[i];
+
+                    this.undoOverlap( collision );
+                    this.elasticBounceSolid( collision );
+
                 }
-                this.is_hit = true;
+                this.color("red");
             },
             function() {
-                this.is_hit === false;
-                console.log( this.color("red") );
+                this.color("green");
             }
         );
-    } 
+
+        this.onHit(
+            "Bounce",
+            function( collisions ) {
+                for( var i in collisions ) {
+                    var collision = collisions[i];
+
+                    this.undoOverlap( collision );
+                    this.elasticBounce( collision );
+
+                }
+                this.color("red");
+            },
+            function() {
+                this.color("green");
+            }
+        );
+    },
+
+    undoOverlap: function( collision ) {
+        this.x += collision.normal.x;
+        this.y += collision.normal.y;
+    },
+
+    elasticBounceSolid: function( collision ) {
+        var other = collision.obj;
+
+        if( collision.normal.x !== 0 ) {
+            this.vx *= -1;
+        }
+        if( collision.normal.y !== 0) {
+            this.vy *= -1;
+        }
+    },
+
+    elasticBounce: function( collision ) {
+        var other = collision.obj;
+
+        if( collision.normal.x !== 0 ) {
+            var temp;
+            temp = this.vx;
+            this.vx = other.vx;
+            other.vx = temp;
+        }
+
+        if( collision.normal.y !== 0 ) {
+            var temp;
+            temp = this.vy;
+            this.vy = other.vy;
+            other.vy = temp;
+        }
+    }
 
 });
