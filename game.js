3a34da5e2baaeb31d058d00b9cf5c1d05f9468f3
Haodong Ruan <haodongruan@dhcp-10-142-158-130.uawifi.arizona.edu>
2014-11-08 09:56:16 -0700

    test

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ Crafty.c("Circle", {
 	},
 
 	draw: function () {
+		if (this.radius>0){
 		var ctx = Crafty.canvas.context;
 		ctx.save();
 		ctx.fillStyle = this.color;
@@ -21,6 +22,9 @@ Crafty.c("Circle", {
 		);
 		ctx.closePath();
 		ctx.fill();
+		ctx.rect(this.x,this.y,this.w,this.h);
+		ctx.stroke();
+		}
 	}
 });
 
@@ -152,33 +156,74 @@ triangle = Crafty.e('enemy,2D, Canvas, Polygon, Color,Collision')
 
 
 var enemyList = [];
-for (var i = 0; i < Crafty.math.randomInt(20, 50); i++) {
-	var radius = Crafty.math.randomInt(5, 20);
+for (var i = 0; i < 5; i++) {
+	var radius = Crafty.math.randomInt(50,70);
 	var ball = Crafty.e('enemy,2D, Canvas,Circle')
 		.attr({
 			x: Crafty.math.randomInt(0,600),
 			y: Crafty.math.randomInt(0, 600),
 			w: radius*2,
 			h: radius*2,
-			vx: 1,
-			vy: 1
+			vx: 0.3,
+			vy: 0.2
 		})
 		.Circle(radius, "green")
 		.bind('EnterFrame', function () {
 			for (var i in enemyList)
 			{
-				if (this!==enemyList[i]){
-				if (Math.abs(this.x-enemyList[i].x)+Math.abs(this.y-enemyList[i].y)<this.radius+enemyList[i].radius)
+				
+				if (this!=enemyList[i]){
+					x1=this.x+this.radius;
+					//console.log("x1= "+x1)
+					x2=enemyList[i].x+enemyList[i].radius;
+					//console.log("x2= "+x2)
+					y1=this.y+this.radius;
+					//console.log("y1= "+y1)
+					y2=enemyList[i].y+enemyList[i].radius;
+					//console.log("y2= "+y1)
+					
+					dist =Math.sqrt(Math.abs(x1-x2)*Math.abs(x1-x2)+Math.abs(y1-y2)*Math.abs(y1-y2))
+					
+				if (dist<=(parseInt (this.radius)+parseInt (enemyList[i].radius)))
 				{
+  
+					this.color="red"
 					this.vx*=-1;
+					this.x-=5*this.vx
 					this.vy*=-1;
+					this.y-=5*this.vy
+					if (this.radius>enemyList[i].radius){
+						this.radius+=0.01;
+						this.w+=0.02;
+						this.h+=0.02;
+						enemyList[i].radius-=0.01;
+						enemyList[i].w-=0.02;
+						enemyList[i].h-=0.02;
+					}
+					else{
+						this.radius-=0.01;
+						this.w-=0.02;
+						this.h-=0.02;
+						enemyList[i].radius+=0.01;
+						enemyList[i].w+=0.02;
+						enemyList[i].h+=0.02;
+					}
+					if (this.radius<=0.1 ){
+						delete this;
+						return;
+					}
+					if( enemyList[i].radius<=0.1){
+						enemyList.splice(i,1);
+					}
 				}
-			}}
+			}
+				
+			}
 			
 			
-			if (this.x > 600 - 100 || this.x < 0)
+			if (this.x > 500 || this.x < 0)
 				this.vx *= -1;
-			if (this.y > 600 - 100 || this.y < 0)
+			if (this.y > 500 || this.y < 0)
 				this.vy *= -1;
 
 			this.x += this.vx;
@@ -190,6 +235,17 @@ for (var i = 0; i < Crafty.math.randomInt(20, 50); i++) {
 	
 	enemyList.push(ball)
 }
+console.log(enemyList)
 
 
 
+
+function boomspeed(obj1,obj2)
+{
+	obj1={
+		dx:0,
+		dy:0,
+		cx:0,
+		cy:0
+	}
+}
