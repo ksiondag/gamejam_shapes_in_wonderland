ab5b295962d6d351995491261bf7a1dba7fb8235
Gregory Ksionda <ksiondag846@gmail.com>
2014-11-08 23:51:21 +0000

    Adding control options.
    Creating player control logic file alice.js.
    
    Defaulting one unit to user control (nicknamed "Alice").
    
    Left-clicking any square will make that Alice, and the old Alice will no
    longer be an Alice.
    
    Right-clicking applies velocity changes to Alice.

---
+++ src/alice.js
@@ -0,0 +1,47 @@
+var alice;
+
+var newAlice = function( eatme ) {
+    if( alice ) {
+        alice.notAlice();
+    }
+    alice = eatme;
+}
+
+Crafty.c( 'Clickable', {
+    init: function() {
+        this.bind("MouseDown", function(e) {
+            if( e.button === Crafty.mouseButtons.LEFT ) {
+                this.addComponent('Alice');
+            }
+        });
+    }
+});
+
+Crafty.c( 'Control', {
+    init: function() {
+        this.bind("MouseDown", function(e) {
+            if( e.button === Crafty.mouseButtons.RIGHT ) {
+                diffX = e.realX - alice.x;
+                diffY = e.realY - alice.y;
+                console.log( (diffX) + ", " + (diffY) );
+                alice.vx += diffX/100;
+                alice.vy += diffY/100;
+            }
+        });
+    }
+});
+
+Crafty.c( 'Alice', {
+    init: function() {
+        newAlice( this );
+
+        this.addComponent('Color');
+        this.color('blue');
+
+    },
+    notAlice: function() {
+        this.color('green');
+        this.removeComponent('Alice', false);
+    }
+});
+
--- src/game.js
+++ src/game.js
@@ -1,51 +1,61 @@
-Crafty.init(600, 600, document.getElementById('game'));
+$( document ).ready( function() {
+    Crafty.init(600, 600, document.getElementById('game'));
 
-var roof = Crafty.e('Solid,Canvas,2D,Color,Collision')
-    .attr({
-        x:0,
-        y:-100,
-        w:600,
-        h:100
-    })
-    .color("#000");
-var floor = Crafty.e('Solid,Canvas,2D,Color,Collision')
-    .attr({
-        x:0,
-        y:600,
-        w:600,
-        h:100
-    })
-    .color("#000");
-
-var left = Crafty.e('Solid,Canvas,2D,Color,Collision')
-    .attr({
-        x:-100,
-        y:0,
-        w:100,
-        h:600
-    })
-    .color("#000");
-var right = Crafty.e('Solid,Canvas,2D,Color,Collision')
-    .attr({
-        x:600,
-        y:0,
-        w:100,
-        h:600
-    })
-    .color("#000");
-
-for (var i = 0; i < 10; i++) {
-	Crafty.e('Canvas,2D,Color,Velocity,Collision,Bounce')
-		.attr({
-            x: Crafty.math.randomInt(0,530),
-            y: Crafty.math.randomInt(0,530),
-            w: Crafty.math.randomInt(50,70),
-            h: Crafty.math.randomInt(50,70)
+    var roof = Crafty.e('Solid,Canvas,2D,Color,Collision')
+        .attr({
+            x:0,
+            y:-100,
+            w:600,
+            h:100
         })
-        .Velocity(
-            Crafty.math.randomInt(-10, 10),
-            Crafty.math.randomInt(-10, 10)
-        )
-        .color( "green" )
-}
+        .color('#000');
+    var floor = Crafty.e('Solid,Canvas,2D,Color,Collision')
+        .attr({
+            x:0,
+            y:600,
+            w:600,
+            h:100
+        })
+        .color('#000');
 
+    var left = Crafty.e('Solid,Canvas,2D,Color,Collision')
+        .attr({
+            x:-100,
+            y:0,
+            w:100,
+            h:600
+        })
+        .color('#000');
+    var right = Crafty.e('Solid,Canvas,2D,Color,Collision')
+        .attr({
+            x:600,
+            y:0,
+            w:100,
+            h:600
+        })
+        .color('#000');
+
+    var clickSpace = Crafty.e('2D,Mouse,Control')
+        .attr({
+            x:0,
+            y:0,
+            w:600,
+            h:600
+        });
+
+    for (var i = 0; i < 10; i++) {
+        Crafty.e('Mouse,Canvas,2D,Color,Velocity,Collision,Bounce,Alice,Clickable')
+            .attr({
+                x: Crafty.math.randomInt(0,530),
+                y: Crafty.math.randomInt(0,530),
+                w: Crafty.math.randomInt(50,70),
+                h: Crafty.math.randomInt(50,70)
+            })
+            .Velocity(
+                0,
+                0,
+                Crafty.math.randomInt(-10, 10),
+                Crafty.math.randomInt(-10, 10)
+            )
+    }
+});
