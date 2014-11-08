de9c30896554a4ccc050942a8b92de3f6ec7b255
Gregory Ksionda <ksiondag846@gmail.com>
2014-11-08 06:39:38 +0000

    Implementing shape components using Crafty shapes
    Shape components are using the "2D" component variables to define
    themselves. This is creating an inaccurate collision box for the
    "Collision" component. Switching to use of built-in polygon hitboxes.
    
    Separating shape component logic from game logic.

--- game.js
+++ game.js
@@ -1,63 +1,14 @@
 Crafty.init(500,350, document.getElementById('game'));
 
-Crafty.c("Circle", {
-    Circle: function(radius) {
-        this.radius = radius;
-        this.w = this.h = radius * 2;
-        
-        return this;
-    },
-    
-    draw: function() {
-        var contex = Crafty.canvas.context;
-        contex.save();
-        contex.fillStyle = this.color();
-        contex.beginPath();
-        contex.arc(
-            this.x + this.radius,
-            this.y + this.radius,
-            this.radius,
-            0,
-            Math.PI * 2
-        );
-        contex.closePath();
-        contex.fill();
-    }
-}); 
-
-Crafty.c("Triangle",{
-    draw:function(){
-        var contex = Crafty.canvas.context;
-        contex.save();
-        contex.fillStyle = this.color();
-        contex.beginPath();
-
-        var mid_x = this.x+this.w/2;
-        contex.lineTo(mid_x, this.y);
-
-        var bottom = this.y + this.h;
-        contex.lineTo(this.x, bottom);
-
-        var right = this.x + this.w;
-        contex.lineTo(right, bottom);
-
-        contex.closePath();
-        contex.stroke();
-        contex.fill();
-    }
-});
-
 square = Crafty.e('2D, Canvas, Color')
     .attr({x: 0, y: 0, w: 100, h:100})
     .color('#000');
 
-triangle = Crafty.e("2D, Canvas, Triangle, Color")
-    .attr({points:[[50,0],[0,100],[100,100]]})
-    .attr({x:50,y:0,w:100,h:100})
+circle = Crafty.e("Canvas, Circle, Color")
+    .Circle( 200, 0, 50 )
     .color('#000');
 
-circle = Crafty.e("2D, Canvas, Circle, Color")
-    .Circle(40)
-    .attr({x:10,y:10})
-    .color('#FFF');
+triangle = Crafty.e("2D, Canvas, Triangle, Color")
+    .Triangle( 100, 0, 100, 100 )
+    .color('#000');
 
---
+++ shapes.js
@@ -0,0 +1,72 @@
+Crafty.c("Circle", {
+    circle: new Crafty.circle( 0, 0, 0 ),
+
+    init: function() {
+        this.bind("EnterFrame", function() {
+            this.draw();
+        })
+    },
+
+    Circle: function( x, y, radius ) {
+        this.circle = new Crafty.circle( x+radius, y+radius, radius );
+
+        return this;
+    },
+    
+    draw: function() {
+        var contex = Crafty.canvas.context;
+        contex.save();
+        contex.fillStyle = this.color();
+        contex.beginPath();
+        contex.arc(
+            this.circle.x,
+            this.circle.y,
+            this.circle.radius,
+            0,
+            Math.PI * 2
+        );
+        contex.closePath();
+        contex.fill();
+    }
+}); 
+
+Crafty.c("Triangle",{
+    triangle: new Crafty.polygon( [[0,0],[0,0],[0,0]] ),
+
+    init:function(){
+        this.bind("EnterFrame", function() {
+            this.draw();
+        })
+    },
+
+    Triangle: function( left, roof, width, height ) {
+        mid_x = left + width/2;
+        right = left + width;
+        floor = roof + height;
+
+        this.triangle = new Crafty.polygon([
+            [mid_x,roof],
+            [left,floor],
+            [right,floor]
+        ]);
+
+        console.log( this.triangle );
+        return this;
+    },
+
+    draw:function(){
+        var contex = Crafty.canvas.context;
+        contex.save();
+        contex.fillStyle = this.color();
+        contex.beginPath();
+
+        for( var i in this.triangle.points ) {
+            var p = this.triangle.points[i];
+            contex.lineTo(p[0], p[1]);
+        }
+
+        contex.closePath();
+        contex.stroke();
+        contex.fill();
+    }
+});
